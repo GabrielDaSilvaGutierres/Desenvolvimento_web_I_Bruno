d1276714899f5eb14bb8c8c5e20f2293c4224251
Gabriel Da Silva Gutierres/Dice <gabriel.gutierres@fatec.sp.gov.br>
2025-05-28 20:23:06 -0300

    Aprendendo JS
    aprendendo JS inicialmente na parte de alert e print com JS/ video sobre JS de Bruno Zolotareff

---
+++ aprendendo java/code.js
@@ -0,0 +1,48 @@
+//atributos - (variaveis)
+
+//JavaScript - não tipado - linguagem de programação dinâmica
+
+//para comentar
+/*
+varias linhas para escrever
+/*
+    Java - fortemente tipada
+    string nome = "Gabriel";
+    int num = 2;
+    double nums = 2.6;
+    long n = 34556464;
+
+    JavaScript - dinâmica
+
+    let nome = "Gabriel"
+    let num = 3.5
+    let n = 3;
+    
+    let = ele altera
+
+    const nome = "Gabriel";
+    const num = 3.5;
+    const n = 3;
+
+    const = não modifica ou altera
+
+    var nome = "Gabriel";
+    var num = 3.5;
+    var n = 3;
+    var = não é seguro
+*/
+//Java
+    //FileSystem.out.println("Olá Java");
+//C#
+    //console.application("Olá C# ");
+//JavaScript
+    //console.log("Olá JavaScript");
+
+//JavaScript
+    //Ações - funções
+    function print(){
+        console.log("Olá JavaScript");
+    }
+    function alert(){
+        console.log("Olá JavaScript");
+    }
